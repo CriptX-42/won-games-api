87e4117b9fd3fdb6d381938615d2e626f5024daa
CriptX-42 <ricardo-svc@live.com>
2021-01-29 01:00:22 -0300

    Preparando a população de dados

--- app/api/game/services/game.js
+++ app/api/game/services/game.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const axios = require("axios");
+const slugify = require("slugify");
 
 async function getGameInfo(slug) {
   const jsdom = require("jsdom");
@@ -15,6 +16,21 @@ async function getGameInfo(slug) {
   };
 }
 
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lowe: true }),
+    });
+  }
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -22,6 +38,9 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(await getGameInfo(products[0].slug));
+    await create(products[0].publisher, "publisher");
+    await create(products[0].developer, "developer");
+
+    // console.log(await getGameInfo(products[0].slug));
   },
 };
