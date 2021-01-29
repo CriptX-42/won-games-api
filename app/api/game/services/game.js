c83fbb7fe85e655f5bcee5902e1546545cd7ab72
CriptX-42 <ricardo-svc@live.com>
2021-01-28 23:07:00 -0300

    Pegando os dados da 'gog.com'

--- app/api/game/services/game.js
+++ app/api/game/services/game.js
@@ -1,8 +1,27 @@
 "use strict";
+
+const axios = require("axios");
+
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+  const description = dom.window.document.querySelector(".description");
+  return {
+    rating: "BR0",
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
+
 module.exports = {
   populate: async (params) => {
-    const cat = await strapi.services.category.find({ name: "Action" });
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
 
-    console.log(cat);
+    console.log(await getGameInfo(products[0].slug));
   },
 };
